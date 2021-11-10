b236020e009dd4840652e461f4af6eab7c0572d1
Rust <rust69@list.ru>
2021-11-10 13:53:56 +0300

    added factorial function

--- lesson3_2.py
+++ lesson3_2.py
@@ -1,8 +1,8 @@
 print('Умножение чисел')
-
+import math
 a = float(input('Введите число A: '))
 b = float(input('Введите число B: '))
-print('Введите действие: \n1 - суммировать; \n2 - вычесть; \n3 - умножить; \n4 - разделить')
+print('Введите действие: \n1 - суммировать; \n2 - вычесть; \n3 - умножить; \n4 - разделить; \n5 - factorial')
 c = int(input())
 
 if c == 1:
@@ -13,6 +13,8 @@ elif c == 3:
 	print(a, '*', b, '=', a*b)
 elif c == 4:
 	print(a, '/', b, '=', a/b)
+elif c == 5:
+	print('factorial', a, '=', math.factorial(int(a)))
 else:
 	print('Такое действие недопустимо!')	
 
